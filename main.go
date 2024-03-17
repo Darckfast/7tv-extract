6b38577cf30fbb7f3c6af971a497c0f47c1da0d9
Victor Costa <victorcosta37@gmail.com>
2024-03-17 09:55:39 -0300

    feat: add ci

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ import (
 )
 
 //go:generate goversioninfo
-
 var hasMagick = true
 
 func main() {
@@ -32,10 +31,52 @@ func main() {
 		tv7UserId = append(tv7UserId, userId)
 	}
 
-	resp, err := http.Get("https://7tv.io/v3/users/twitch/" + tv7UserId[0])
+	shortEmoteList, emotes := GetEmoteList(tv7UserId[0])
+	if shortEmoteList == nil {
+		return
+	}
+
+	stringContent, _ := json.MarshalIndent(shortEmoteList, "", " ")
+
+	os.MkdirAll(filepath.Join(emotes.Username, "emotes"), os.ModePerm)
+	os.WriteFile(filepath.Join(emotes.Username, "emotes_list.json"), stringContent, os.ModePerm)
+
+	threads := runtime.NumCPU()
+
+	log.Printf("Using %d threads\n", threads)
+	limiter := make(chan int, threads)
+
+	CheckForMagick()
+
+	var wg sync.WaitGroup
+
+	for index, shortEmote := range *shortEmoteList {
+		limiter <- 1
+		wg.Add(1)
+
+		DownloadEmote(
+			&shortEmote,
+			index,
+			len(*shortEmoteList),
+			emotes.Username,
+			limiter,
+			&wg,
+		)
+	}
+	wg.Wait()
+
+	log.Println("Completed", emotes.User.Username, tv7UserId)
+
+	if !hasMagick {
+		log.Println("For auto conversion first install ImageMagick https://imagemagick.org/script/download.php")
+	}
+}
+
+func GetEmoteList(userId string) (*[]ShortEmoteList, *Emotes) {
+	resp, err := http.Get("https://7tv.io/v3/users/twitch/" + userId)
 	if err != nil {
 		log.Println(err.Error())
-		return
+		return nil, nil
 	}
 
 	defer resp.Body.Close()
@@ -59,74 +100,59 @@ func main() {
 		})
 	}
 
-	stringContent, _ := json.MarshalIndent(shortEmoteList, "", " ")
-
-	os.MkdirAll(filepath.Join(emotes.Username, "emotes"), os.ModePerm)
-	os.WriteFile(filepath.Join(emotes.Username, "emotes_list.json"), stringContent, os.ModePerm)
-
-	threads := runtime.NumCPU()
-
-	log.Printf("Using %d threads\n", threads)
-	limiter := make(chan int, threads)
-
-	var wg sync.WaitGroup
-
-	CheckForMagick()
-
-	for index, shortEmote := range shortEmoteList {
-		limiter <- 1
-		wg.Add(1)
-
-		go func(shortEmote ShortEmoteList, index int) {
-			log.Printf("%d/%d Downloading emote %s\n", index+1, len(shortEmoteList), shortEmote.EmoteName)
-			resp, err := http.Get(shortEmote.FullUrl)
-			if err != nil {
-				log.Println("Error making the request", err.Error())
-				<-limiter
-				wg.Done()
-				return
-			}
-
-			defer resp.Body.Close()
-
-			fileName := filepath.Join(
-				emotes.Username,
-				"emotes",
-				shortEmote.EmoteName+"."+shortEmote.Extension)
-
-			out, err := os.Create(fileName)
-			if err != nil {
-				log.Println("Error creating file", err.Error())
-				<-limiter
-				wg.Done()
-				return
-			}
-
-			defer func() {
-				out.Close()
-				log.Printf("%d/%d Converting emote %s", index+1, len(shortEmoteList), shortEmote.EmoteName)
-				ConvertFile(fileName, shortEmote.IsAnimated, limiter)
-				os.Remove(fileName)
-
-				wg.Done()
-			}()
-
-			io.Copy(out, resp.Body)
-		}(shortEmote, index)
-	}
-	wg.Wait()
-
-	log.Println("Completed", emotes.User.Username, tv7UserId)
-
-	if !hasMagick {
-		log.Println("For auto conversion first install ImageMagick https://imagemagick.org/script/download.php")
-	}
+	return &shortEmoteList, &emotes
 }
 
-func CheckForMagick() {
+func DownloadEmote(
+	shortEmote *ShortEmoteList,
+	index int,
+	total int,
+	username string,
+	limiter chan int,
+	wg *sync.WaitGroup,
+) {
+	log.Printf("%d/%d Downloading emote %s\n", index+1, total, shortEmote.EmoteName)
+	resp, err := http.Get(shortEmote.FullUrl)
+	if err != nil {
+		log.Println("Error making the request", err.Error())
+		<-limiter
+		wg.Done()
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fileName := filepath.Join(
+		username,
+		"emotes",
+		shortEmote.EmoteName+"."+shortEmote.Extension)
+
+	out, err := os.Create(fileName)
+	if err != nil {
+		log.Println("Error creating file", err.Error())
+		<-limiter
+		wg.Done()
+		return
+	}
+
+	defer func() {
+		out.Close()
+		log.Printf("%d/%d Converting emote %s", index+1, total, shortEmote.EmoteName)
+		ConvertFile(fileName, shortEmote.IsAnimated, limiter)
+		os.Remove(fileName)
+
+		wg.Done()
+	}()
+
+	io.Copy(out, resp.Body)
+}
+
+func CheckForMagick() bool {
 	_, err := exec.LookPath("magick")
 	hasMagick = err == nil
 	exec.Command("set", "MAGICK_OCL_DEVICE=true")
+
+	return hasMagick
 }
 
 func ConvertFile(fileName string, isAnimated bool, limiter chan int) {
